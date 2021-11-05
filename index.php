4db0c9e7fff1639972fcaac0ee29358df79ce44b
Nicolás Montiel <nicolasmontielf@gmail.com>
2021-11-05 13:41:16 -0300

    Mejores mensajes y bucle

--- index.php
+++ index.php
@@ -13,71 +13,75 @@ $directorioFiltrado = array_values(
     })
 );
 
-echo "Actuales archivos excel: " . PHP_EOL;
-foreach ($directorioFiltrado as $index => $filtrado) {
-    $num = $index + 1;
-    echo "{$num}- {$filtrado}" . PHP_EOL;
-}
+while (true) {
+    echo "Actuales archivos excel: " . PHP_EOL;
+    foreach ($directorioFiltrado as $index => $filtrado) {
+        $num = $index + 1;
+        echo "{$num}- {$filtrado}" . PHP_EOL;
+    }
 
-$input = readline("Seleccione un archivo: ");
-echo PHP_EOL . "Se está procesando el excel" . PHP_EOL;
+    $input = readline("Seleccione un archivo: ");
+    echo PHP_EOL . "Se está procesando el excel..." . PHP_EOL;
 
-$archivo = (explode(".", $directorioFiltrado[($input-1)]))[0];
+    $archivo = (explode(".", $directorioFiltrado[($input-1)]))[0];
 
-// Toda la parte de creación de excel
-try {
-    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-    $spreadsheet = $reader->load("{$directorio}/{$archivo}.xlsx");
-    $worksheet  = $spreadsheet->getActiveSheet();
+    // Toda la parte de creación de excel
+    try {
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        $spreadsheet = $reader->load("{$directorio}/{$archivo}.xlsx");
+        $worksheet  = $spreadsheet->getActiveSheet();
 
-    $isHeader = true; // contador de filas
-    $headers = array(); // Array que va a contener los headers
-    $arr = array(); // Array master
+        $isHeader = true; // contador de filas
+        $headers = array(); // Array que va a contener los headers
+        $arr = array(); // Array master
 
-    // Foreach que recorre las filas
-    foreach ($worksheet->getRowIterator() as $row) {
-        $cellIterator = $row->getCellIterator();
-        $cellIterator->setIterateOnlyExistingCells(FALSE);
+        // Foreach que recorre las filas
+        foreach ($worksheet->getRowIterator() as $row) {
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(FALSE);
 
-        $index = 0;
-        $aux = [];
+            $index = 0;
+            $aux = [];
 
-        // Foreach que recorre cada celda de esta fila
-        foreach ($cellIterator as $cell) {
-            // Si el contador está , es el header, lo guardamos en el array
-            if ($isHeader) {
-                array_push($headers, $cell->getValue());
-            } else {
-                $aux[$headers[$index]] = $cell->getValue();
+            // Foreach que recorre cada celda de esta fila
+            foreach ($cellIterator as $cell) {
+                // Si el contador está , es el header, lo guardamos en el array
+                if ($isHeader) {
+                    array_push($headers, $cell->getValue());
+                } else {
+                    $aux[$headers[$index]] = $cell->getValue();
+                }
+                $index++;
             }
-            $index++;
+
+            if (!$isHeader) {
+                array_push($arr, $aux);
+            }
+            
+            $isHeader = false;
         }
 
-        if (!$isHeader) {
-            array_push($arr, $aux);
+        echo "Ahora lo estamos convirtiendo a txt..." . PHP_EOL;
+
+        // Cargamos el texto
+        $texto = "";
+        foreach ($arr as $fila) {
+            $texto .= "[" . PHP_EOL;
+            foreach ($fila as $index => $cell) {
+                $texto .= "\t '{$index}' => '{$cell}', " . PHP_EOL;
+            }
+            $texto .= "]," . PHP_EOL;
         }
-        
-        $isHeader = false;
+
+        // Guardamos en un text
+        $nombreTxt = $slugify->slugify($archivo);
+        $txt = fopen("./result/{$nombreTxt}.txt", "w");
+        fwrite($txt, $texto);
+
+        echo "¡Se ha finalizado el proceso!" . PHP_EOL;
+        echo "Si desea salir, por favor cierre la consola." . PHP_EOL;
+    } catch (\Exception $e) {
+        echo "Ha ocurrido un error" . PHP_EOL;
+        echo $e->getMessage();
     }
-
-    // Cargamos el texto
-    $texto = "";
-    foreach ($arr as $fila) {
-        $texto .= "[" . PHP_EOL;
-        foreach ($fila as $index => $cell) {
-            $texto .= "\t '{$index}' => '{$cell}', " . PHP_EOL;
-        }
-        $texto .= "]," . PHP_EOL;
-    }
-
-    // Guardamos en un text
-    $nombreTxt = $slugify->slugify($archivo);
-    $txt = fopen("./result/{$nombreTxt}.txt", "w");
-    fwrite($txt, $texto);
-
-    echo "Se ha finalizado el proceso..." . PHP_EOL;
-    exit;
-} catch (\Exception $e) {
-    echo "Ha ocurrido un error" . PHP_EOL;
-    echo $e->getMessage();
 }
